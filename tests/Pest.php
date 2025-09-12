4559fa08f3c0db94e39bd8f044c0f92212875c98
Gustavo Muniz dos anjos <contact@gustavoanjos.com>
2025-09-12 09:42:43 -0300

    Add CreatesApplication trait and clean up test setup

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,9 +11,7 @@
 |
 */
 
-pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature');
+pest()->extend(Tests\TestCase::class)->in("Feature");
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +24,7 @@ pest()->extend(Tests\TestCase::class)
 |
 */
 
-expect()->extend('toBeOne', function () {
+expect()->extend("toBeOne", function () {
     return $this->toBe(1);
 });
 
