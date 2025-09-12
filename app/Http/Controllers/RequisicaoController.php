611bc831a67c55b35103e642b134e59bcb956d6e
Gustavo Muniz dos anjos <contact@gustavoanjos.com>
2025-09-12 08:51:28 -0300

    Use single quotes for strings in PHP and YAML configs
    Replace Prettier with Biome in lint workflow
    
    Remove unused security-basic GitHub Actions workflow
    
    Fix indentation in package.json devDependencies

--- app/Http/Controllers/RequisicaoController.php
+++ app/Http/Controllers/RequisicaoController.php
@@ -471,7 +471,7 @@ class RequisicaoController extends Controller
 
         $filename = basename($requisicao->anexo);
 
-        return Storage::disk("public")->download($requisicao->anexo, basename($requisicao->anexo));
+        return Storage::disk('public')->download($requisicao->anexo, basename($requisicao->anexo));
     }
 
     /**
